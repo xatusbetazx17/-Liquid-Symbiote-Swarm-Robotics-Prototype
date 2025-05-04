d0d301e90a8314133f9ee3cec6ed67f009a7032f
xatusbetazx17 <xatusbetazx17@gmail.com>
2025-05-04 18:24:14 -0400

    Create config.h

---
+++ firmware/ArduinoBots/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Unique per-bot (set in Bot.ino)
+#define BOT_ID           1
+
+// BLE settings
+#define BLE_DEVICE_NAME  "SymBot"
+
+// Simulated energy
+#define MAX_ENERGY       100
+
+#endif
